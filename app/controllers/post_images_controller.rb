4e1b9047ffd72579cd4f6ec5c6963a24eeab7d6c
shirahamakaho <112605721+shirahamakaho@users.noreply.github.com>
2022-09-12 00:51:00 +0000

    niko^^

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -11,24 +11,24 @@ class PostImagesController < ApplicationController
     redirect_to post_images_path
   end
 
+  def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to post_images_path
+  end
+
   def index
     @post_images = PostImage.all
   end
-
-  def destroy
-　  @post_image = PostImage.find(params[:id])
-　  @post_image.destroy
-　  redirect_to post_images_path
-　end
 
   def show
     @post_image = PostImage.find(params[:id])
   end
 
   private
+
    def post_image_params
     params.require(:post_image).permit(:shop_name,:image,:caption)
    end
-  end
 
 end
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -9,4 +9,5 @@ class PostImage < ApplicationRecord
     end
     image
   end
+
 end
